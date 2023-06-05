36a69f6638cbb4d3608b83ca5987aae9da0247a0
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-06-05 20:55:46 +0200

    Fix for async logout method

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,55 @@
-const router = require('express').Router();
-const passport = require('passport');
+const router = require("express").Router();
+const passport = require("passport");
 
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+    res.render("index");
 });
 
-router.get('/signup', (req, res, next) => {
-  res.render('signup');
+router.get("/signup", (req, res, next) => {
+    res.render("signup");
 });
 
-router.post('/signup', passport.authenticate('local-signup', {
-  successRedirect: '/profile',
-  failureRedirect: '/signup',
-  failureFlash: true
-})); 
+router.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+        successRedirect: "/profile",
+        failureRedirect: "/signup",
+        failureFlash: true,
+    })
+);
 
-router.get('/signin', (req, res, next) => {
-  res.render('signin');
+router.get("/signin", (req, res, next) => {
+    res.render("signin");
 });
 
+router.post(
+    "/signin",
+    passport.authenticate("local-signin", {
+        successRedirect: "/profile",
+        failureRedirect: "/signin",
+        failureFlash: true,
+    })
+);
 
-router.post('/signin', passport.authenticate('local-signin', {
-  successRedirect: '/profile',
-  failureRedirect: '/signin',
-  failureFlash: true
-}));
-
-router.get('/profile',isAuthenticated, (req, res, next) => {
-  res.render('profile');
+router.get("/profile", isAuthenticated, (req, res, next) => {
+    res.render("profile");
 });
 
-router.get('/logout', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
+router.get("/logout", function (req, res, next) {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
 });
-
 
 function isAuthenticated(req, res, next) {
-  if(req.isAuthenticated()) {
-    return next();
-  }
+    if (req.isAuthenticated()) {
+        return next();
+    }
 
-  res.redirect('/')
+    res.redirect("/");
 }
 
 module.exports = router;
